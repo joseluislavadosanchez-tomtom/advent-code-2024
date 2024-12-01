398c1bea723b9eb4f8b9df3f6e31ee041e68d9c1
joseluislavadosanchez-tomtom <joseluis.lavadosanchez@tomtom.com>
2024-12-01 18:56:15 +0000

    Refactor first star

--- problems/01_12_2024/first_start.py
+++ problems/01_12_2024/first_start.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def read_input() -> tuple[list, list]:
+def read_input() -> tuple[list[int], list[int]]:
     list1, list2 = [], []
     for line in sys.stdin:
         num1, num2 = map(int, line.split())
@@ -10,7 +10,12 @@ def read_input() -> tuple[list, list]:
     return list1, list2
 
 
+def result(list1: list[int], list2: list[int]) -> int:
+    list1.sort()
+    list2.sort()
+    return sum([abs(x - y) for x, y in zip(list1, list2)])
+
+
 if __name__ == "__main__":
     list1, list2 = read_input()
-    result = sum((abs(x - y) for x, y in zip(sorted(list1), sorted(list2))))
-    print(result)
+    print(result(list1, list2))
