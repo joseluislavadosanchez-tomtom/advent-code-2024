b34e6d35343890e1891a3b4670ea7d0dfc510674
joseluislavadosanchez-tomtom <joseluis.lavadosanchez@tomtom.com>
2024-12-01 19:03:52 +0000

    Refactor read_input function to specify list types and add result function for improved clarity

--- problems/01_12_2024/second_start.py
+++ problems/01_12_2024/second_start.py
@@ -2,7 +2,7 @@ import sys
 from collections import Counter
 
 
-def read_input() -> tuple[list, list]:
+def read_input() -> tuple[list[int], list[int]]:
     list1, list2 = [], []
     for line in sys.stdin:
         num1, num2 = map(int, line.split())
@@ -11,8 +11,12 @@ def read_input() -> tuple[list, list]:
     return list1, list2
 
 
+def result(list1: list[int], list2: list[int]) -> int:
+    count_list2 = Counter(list2)
+    return sum(x * count_list2.get(x, 0) for x in list1)
+
+
 if __name__ == "__main__":
     list1, list2 = read_input()
     count_list2 = Counter(list2)
-    result = sum(x * count_list2.get(x, 0) for x in list1)
-    print(result)
+    print(result(list1, list2))
