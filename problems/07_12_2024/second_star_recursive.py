1bf501f744d5ec351ad4ef3a2f9f112efca896e9
joseluislavadosanchez-tomtom <joseluis.lavadosanchez@tomtom.com>
2024-12-07 18:00:02 +0000

    Add second_star_recursive.py for 07_12_2024 challenge with input reading and equation evaluation logic

---
+++ problems/07_12_2024/second_star_recursive.py
@@ -0,0 +1,63 @@
+import math
+import sys
+
+
+def read_line():
+    # Return a line from the input using a generator
+    for line in sys.stdin:
+        # parse line first a int then : and then a list of ints
+        result, values = line.split(":")
+        values = list(map(int, values.split()))
+        yield int(result), values
+
+
+def evaluate_equation(
+    target: int,
+    numbers: list[int],
+    index: int = 1,
+    current_value: int | None = None,
+) -> bool:
+    if current_value is None:
+        current_value = numbers[0]
+
+    if index == len(numbers):
+        return current_value == target
+
+    # Multiplication
+    multiplication_value = current_value * numbers[index]
+    if multiplication_value <= target and evaluate_equation(
+        target, numbers, index + 1, multiplication_value
+    ):
+        return True
+
+    # Concatenation
+    concatenated_value = (
+        current_value * (10 ** (1 + int(math.log10(numbers[index])))) + numbers[index]
+        if numbers[index] > 0
+        else current_value * 10 + numbers[index]
+    )
+    if concatenated_value <= target and evaluate_equation(
+        target, numbers, index + 1, concatenated_value
+    ):
+        return True
+
+    # Addition
+    add_value = current_value + numbers[index]
+    if add_value <= target and evaluate_equation(target, numbers, index + 1, add_value):
+        return True
+
+    return False
+
+
+def result() -> int:
+    final_sum = 0
+
+    for target, values in read_line():
+        if evaluate_equation(target, values):
+            final_sum += target
+
+    return final_sum
+
+
+if __name__ == "__main__":
+    print(result())
